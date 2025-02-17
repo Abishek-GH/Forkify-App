ae8f7209a39392cad507cf6a4471e6fa48c2b800
Abishek-GH <abishek0149@gmail.com>
2025-02-17 21:38:28 +0530

    Add Fetch Recipe Function

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// Show Recipe
+
+const showRecipe = async function (id) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+    );
+    const data = await response.json();
+    if (!response.ok)
+      throw new Error(`Error: ${data.message}, Status: ${response.status}`);
+
+    console.log(data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+showRecipe('5ed6604591c37cdc054bc886');
