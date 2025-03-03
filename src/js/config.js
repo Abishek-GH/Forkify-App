7db597d24f6fdbb6f3a141f78392daa7290d955e
Abishek-GH <abishek0149@gmail.com>
2025-03-04 02:02:45 +0530

    Final Build in Master Branch

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const API_KEY = 'API_KEY';
+export const API_KEY = '0b455ad9-4751-43a2-a243-77f50565abeb';
 export const MODAL_CLOSE_SEC = 1;
