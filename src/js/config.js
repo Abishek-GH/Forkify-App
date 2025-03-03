43c08a3db5b4ac7b9b516361c97ed8d86e893524
Abishek-GH <abishek0149@gmail.com>
2025-03-04 03:20:42 +0530

    Final Push and Deploy to Netlify

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const API_KEY = '0b455ad9-4751-43a2-a243-77f50565abeb';
-export const MODAL_CLOSE_SEC = 1;
+export const MODAL_CLOSE_SEC = 0.5;
